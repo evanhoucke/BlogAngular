6b3dd039eb347a4fb8b8325bffa881ee9e015e84
Emmanuel VANHOUCKE <emmanuel.vanhoucke@a-sis.com>
2014-01-09 13:04:53 +0100

    Initial project

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var blog = angular.module('blog', ['ngResource']);
+var blog = angular.module('blog', ['ngResource','articleControllers','articlesServices']);
 
 blog.config(function($routeProvider) {
-  $routeProvider.
-      when('/', {
-          templateUrl: 'views/home.html'
-      }).
-      when('/about', {
-          templateUrl: 'views/about.html'
-      });
+  $routeProvider.when('/', {templateUrl: 'views/home.html'});
+  $routeProvider.when('/angularjs', {templateUrl: 'views/article-list.html',controller: 'ArticleListCtrl'});
+  $routeProvider.when('/about', {templateUrl: 'views/about.html'});
+  $routeProvider.when('/404', {templateUrl: 'views/404.html'});
+  $routeProvider.otherwise({redirectTo: '/404'});
 });
